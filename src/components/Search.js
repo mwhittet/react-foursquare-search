99305bfa84b2135b5c3e5fcd47b08ebf8531bbd9
Michael Whittet <5453237+mwhittet@users.noreply.github.com>
2017-06-26 00:10:40 +0100

    Clear the input when a search is made + constructor added in.

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import '../css/app.css';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.submitSearch = this.submitSearch.bind(this);
+  }
+
   render() {
     return (
-      <form className="search" onSubmit={ (e) => this.submitSearch(e) }>
-        <input type="text" required placeholder="Please enter a location" ref={ (input) => { this.searchInput = input} } />
-        <button type="submit">Search</button>
-      </form>
+      <div>
+        <form className="search" onSubmit={ (e) => this.submitSearch(e) }>
+          <input type="text" required placeholder="Please enter a location" ref={ (input) => { this.searchInput = input} } />
+          <button type="submit">Search</button>
+        </form>
+        
+        <p>Your results:</p>
+        <ul id="search-results"></ul>
+      </div>
     );
   } // render
 
@@ -18,6 +28,8 @@ class Search extends Component {
     var cleanValue = searchString.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, ''); // Next filter any special characters out.
     var finalValue = cleanValue.replace(/\s/g, '+'); // Join words with a + to make the query more friendly.
     console.log('Test input: ' +  finalValue);
+
+    document.getElementsByTagName('form')[0].reset(); // Clear the input for a new search.
   }
 }
 
